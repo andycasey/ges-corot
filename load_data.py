af91ce1a1e0666ba5a851349b8cf3beb8f694fe4
Andy Casey <andycasey@gmail.com>
2014-07-01 13:46:08 +0100

    All data now in data/'

--- load_data.py
+++ load_data.py
@@ -3,30 +3,32 @@ import logging
 import numpy as np
 import pyfits
 
-__all__ = ["all_node_results", "benchmarks", "initial_parameters", "validate_all"]
+__all__ = ["all_node_results", "benchmark_truths", "initial_parameters", "validate_all"]
 
-all_node_results = {
-    "LUMBA-1": (pyfits.open("node-data/lumba/GES_CoRoT_25May_LUMBA.fits/GES_CoRoT_25May_LUMBA.fits")[1].data, None, "GIRAFFE"),
-    "LUMBA-2": (pyfits.open("node-data/lumba/GES_CoRoT_25May_LUMBA.fits/GES_CoRoT_25May_LUMBA.fits")[2].data, None, "GIRAFFE"),
-    "BOLOGNA": (pyfits.open("node-data/bologna/Bologna_GES_CoRoT/Bologna_GES_CoRoT/GES_CoRoT_25May2014_Bologna.fits")[1].data, None, "UVES"),
-    "VILNIUS": (pyfits.open("node-data/vilnius/GES_Corot_iteration1_Vilnius.v11-corrected.fits")[1].data, None, "UVES"),
-    "EPINARBO-UVES": (pyfits.open("node-data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014.fits")[1].data, None, "UVES"),
-    "EPINARBO-GIRAFFE": (pyfits.open("node-data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014.fits")[1].data, None, "GIRAFFE"),
-}
+def load_benchmarks(filename="benchmarks.txt"):
+    """ Loads the Gaia benchmarks and expected stellar parameters as
+    a record array """
 
-bm_object, bm_teff, bm_u_teff, bm_logg, bm_u_logg, bm_z = np.loadtxt("benchmarks.txt",
-    usecols=(2, 3, 4, 5, 6, 7), dtype=str, unpack=True)
+    data = np.loadtxt(filename, dtype=str)
+    max_filenames_len, max_cname_len, max_object_len = (max(map(len, data[:, i])) for i in xrange(3))
 
-# Convert data types
-bm_teff, bm_u_teff, bm_logg, bm_u_logg, bm_z = map(lambda each: np.array(each, dtype=float),
-    [bm_teff, bm_u_teff, bm_logg, bm_u_logg, bm_z])
+    benchmarks = np.core.records.fromarrays(data.T,
+        names=["FILENAME", "CNAME", "Object", "TEFF",
+            "u_TEFF", "LOGG", "u_LOGG", "FEH"],
+        formats=[
+            "|S{0:.0f}".format(max_filenames_len),
+            "|S{0:.0f}".format(max_cname_len),
+            "|S{0:.0f}".format(max_object_len),
+            "f8", "f8", "f8", "f8", "f8"])
 
-# Create record array
-benchmark_truths = np.core.records.fromarrays([bm_object, bm_teff, bm_u_teff, bm_logg, bm_u_logg, bm_z],
-    names=["OBJECT", "TEFF", "u_TEFF", "LOGG", "u_LOGG", "FeH"],
-    formats=["|S10"] + ["f8"] * 5)
+    return benchmarks
 
-initial_parameters = pyfits.open("node-data/initial_parameters.fits")[1].data
+def load_node_data(filename, ext):
+    data = pyfits.open(filename)[ext].data
+
+    # Sort by CNAME
+    indices = np.argsort(data["CNAME"])
+    return data[indices]
 
 def validate(node):
     """
@@ -59,5 +61,40 @@ def validate(node):
 
     return all_ok
 
+
 def validate_all():
     return map(validate, all_node_results.keys())
+
+# Functions defined. Load the data.
+all_node_results = {
+    "ARCETRI-UVES": (load_node_data("data/arcetri/GES_CoRoT_Arcetri_iter1.fits", 1), None, "UVES"),
+    "ARCETRI-GIRAFFE": (load_node_data("data/arcetri/GES_CoRoT_Arcetri_iter1.fits", 1), None, "GIRAFFE"),
+    "IACAIP": (load_node_data("data/iacaip/GES_CoRoT_IACAIP_v0.fits", 1), None, "UVES"),
+    "PHOTOMETRY-U": (load_node_data("data/photometry/GES_CoRoT_PhotTeff.fits", 1), None, "UVES"),
+    "PHOTOMETRY-G": (load_node_data("data/photometry/GES_CoRoT_PhotTeff.fits", 1), None, "GIRAFFE"),
+    "LUMBA-EXCITATION": (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_ExcTeff.fits", 1), None, "GIRAFFE"),
+    "LUMBA-H-ALPHA": (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_HalphaTeff.fits", 1), None, "GIRAFFE"),
+    "LUMBA-NLTE": (load_node_data("data/lumba-nlte/GES_iDR3_WG11_LUMBA-CoRoT.fits", 1), None, "UVES"),
+    "SOUSA-GIRAFFE": (load_node_data("data/sousa/Results_line_ratio_sousa.fits", 1), None, "GIRAFFE"),
+    "SOUSA-UVES": (load_node_data("data/sousa/Results_line_ratio_sousa.fits", 1), None, "UVES"),
+    "BOLOGNA": (load_node_data("data/bologna/Bologna_GES_CoRoTv2/GES_CoRoT_25May2014_Bologna_NEW.fits", 1), None, "UVES"),
+    "VILNIUS": (load_node_data("data/vilnius/GES_Corot_iteration1_Vilnius.v11-corrected.fits", 1), None, "UVES"),
+    "EPINARBO-UVES": (load_node_data("data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014-v2.fits", 1), None, "UVES"),
+    "EPINARBO-GIRAFFE": (load_node_data("data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014-v2.fits", 1), None, "GIRAFFE"),
+}
+
+bm_object, bm_teff, bm_u_teff, bm_logg, bm_u_logg, bm_z = np.loadtxt("data/benchmarks.txt",
+    usecols=(2, 3, 4, 5, 6, 7), dtype=str, unpack=True)
+
+# Convert data types
+bm_teff, bm_u_teff, bm_logg, bm_u_logg, bm_z = map(lambda each: np.array(each, dtype=float),
+    [bm_teff, bm_u_teff, bm_logg, bm_u_logg, bm_z])
+
+# Create record array
+benchmark_truths = np.core.records.fromarrays([bm_object, bm_teff, bm_u_teff, bm_logg, bm_u_logg, bm_z],
+    names=["OBJECT", "TEFF", "u_TEFF", "LOGG", "u_LOGG", "FeH"],
+    formats=["|S10"] + ["f8"] * 5)
+
+initial_parameters = pyfits.open("data/initial_parameters.fits")[1].data
+
+validate_all()
