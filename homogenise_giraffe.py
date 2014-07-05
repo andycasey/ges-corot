19e86eb155b58916bb85de43fc62aad4482d6b19
Andy Casey <andycasey@gmail.com>
2014-07-05 18:42:01 +0100

    Cleaned up scripts

--- homogenise_giraffe.py
+++ homogenise_giraffe.py
@@ -5,36 +5,53 @@
 
 __author__ = "Andy Casey (arc@ast.cam.ac.uk)"
 
+import os
+from hashlib import md5
+from time import ctime
+
 import corot
 import visualise
 from load_data import benchmark_truths, giraffe_results
 
 # Run name.
 run_name = "giraffe-{0}".format(md5(os.path.basename(ctime()).encode("utf-8")).hexdigest())
+run_name = None
 
 # Draw all the node measurements first
 fig_node_all_measurements = visualise.node_all_measurements(giraffe_results)
+fig_node_all_measurements.savefig("plots/giraffe-all-measurements.pdf")
 
 # Calibrate the model
 model = corot.calibrate_noise_model(benchmark_truths, giraffe_results, run_name=run_name)
 
 # Draw the node uncertainties
 fig_node_uncertainties = visualise.inferred_node_uncertainties(model)
+fig_node_uncertainties.savefig("plots/giraffe-inferred-uncertainties.pdf")
 
 # Do a cross-validation of all the benchmark stars
 cv_measurements, cv_uncertainties = corot.cross_validate(benchmark_truths, giraffe_results)
 
 # Do some post-processing of the cross-validation results
 fig_boxplots = visualise.boxplots(benchmark_truths, model, cv_measurements, cv_uncertainties)
+fig_boxplots.savefig("plots/giraffe-benchmark-boxplot.pdf")
 fig_node_benchmark_measurements = visualise.node_benchmark_measurements(benchmark_truths, model,
     cv_measurements, cv_uncertainties)
+fig_node_benchmark_measurements.savefig("plots/giraffe-benchmark-measurements.pdf")
 
 # Homogenise all the data.
-homogenised_results = corot.homogenise_all(giraffe_results, model)
+giraffe_cnames, giraffe_snr, giraffe_homogenised_results = corot.homogenise_all(giraffe_results, model)
+
+# For stars with multiple measurements, plot the results.
+fig_multiple_measurements = visualise.repeat_measurements(giraffe_cnames, giraffe_snr, giraffe_homogenised_results)
+fig_multiple_measurements.savefig("plots/giraffe-multiple-measurements.pdf")
 
 # Visualise the homogenised results in context of the initial node results as a corner plot.
-
-# Plot the homogenised uncertainties against S/N, magnitude
-
+fig_uncertainties_vs_snr = visualise.snr_vs_uncertainties(giraffe_snr, giraffe_homogenised_results)
+fig_uncertainties_vs_snr.savefig("plots/giraffe-snr-vs-uncertainties.pdf")
 
 # Create a GES/CoRoT-standardised FITS table with the homogenised results.
+hdulist = corot.update_table("results/homogenised_parameters.fits",
+    giraffe_cnames, giraffe_homogenised_results, setup="GIRAFFE")
+hdulist.writeto("results/homogenised_parameters.fits", clobber=True)
+
+# Visualise the homogenised results in context of the initial node results as a corner plot.
--- homogenise_uves.py
+++ homogenise_uves.py
@@ -5,36 +5,49 @@
 
 __author__ = "Andy Casey (arc@ast.cam.ac.uk)"
 
+import os
+from hashlib import md5
+from time import ctime
+
 import corot
 import visualise
 from load_data import benchmark_truths, uves_results
 
 # Run name.
 run_name = "uves-{0}".format(md5(os.path.basename(ctime()).encode("utf-8")).hexdigest())
+run_name = None
 
 # Draw all the node measurements first
 fig_node_all_measurements = visualise.node_all_measurements(uves_results)
+fig_node_all_measurements.savefig("plots/uves-all-measurements.pdf")
 
 # Calibrate the model
 model = corot.calibrate_noise_model(benchmark_truths, uves_results, run_name=run_name)
 
 # Draw the node uncertainties
 fig_node_uncertainties = visualise.inferred_node_uncertainties(model)
+fig_node_uncertainties.savefig("plots/uves-inferred-uncertainties.pdf")
 
 # Do a cross-validation of all the benchmark stars
 cv_measurements, cv_uncertainties = corot.cross_validate(benchmark_truths, uves_results)
 
 # Do some post-processing of the cross-validation results
 fig_boxplots = visualise.boxplots(benchmark_truths, model, cv_measurements, cv_uncertainties)
+fig_boxplots.savefig("plots/uves-benchmark-boxplot.pdf")
 fig_node_benchmark_measurements = visualise.node_benchmark_measurements(benchmark_truths, model,
     cv_measurements, cv_uncertainties)
+fig_node_benchmark_measurements.savefig("plots/uves-benchmark-measurements.pdf")
 
 # Homogenise all the data.
-homogenised_results = corot.homogenise_all(uves_results, model)
+uves_cnames, uves_snr, uves_homogenised_results = corot.homogenise_all(uves_results, model)
 
-# Visualise the homogenised results in context of the initial node results as a corner plot.
-
-# Plot the homogenised uncertainties against S/N, magnitude
-
+# Plot the uncertainties as a function of S/N.
+fig_uncertainties_vs_snr = visualise.snr_vs_uncertainties(uves_snr, uves_homogenised_results)
+fig_uncertainties_vs_snr.savefig("plots/uves-snr-vs-uncertainties.pdf")
 
 # Create a GES/CoRoT-standardised FITS table with the homogenised results.
+hdulist = corot.update_table("results/homogenised_parameters.fits",
+    uves_cnames, uves_homogenised_results, setup="UVES")
+hdulist.writeto("results/homogenised_parameters.fits", clobber=True)
+
+# Visualise the homogenised results in context of the initial node results as a corner plot.
