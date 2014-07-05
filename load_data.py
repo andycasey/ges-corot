5b11949f30bb45546fdf7924e0f0134d2c15ef38
Andy Casey <andycasey@gmail.com>
2014-07-05 18:41:47 +0100

    Cleaned the input data from all the nodes, based on TECH flags

--- load_data.py
+++ load_data.py
@@ -41,7 +41,7 @@ def load_node_data(filename, ext):
     return data[indices]
 
 
-def validate(node):
+def validate(node, all_node_results):
     """
     Check that the new node parameters are not the original input parameters.
     """
@@ -77,48 +77,26 @@ def validate(node):
 
     return all_ok
 
-
-def validate_all():
-    return map(validate, all_node_results.keys())
-
-
 # Functions defined. Load the data.
-all_node_results = {
-    "ARCETRI-UVES": (load_node_data("data/arcetri/GES_CoRoT_Arcetri_iter1.fits", 1), None, "UVES"),
-    "ARCETRI-GIRAFFE": (load_node_data("data/arcetri/GES_CoRoT_Arcetri_iter1.fits", 1), None, "GIRAFFE"),
-    "IACAIP": (load_node_data("data/iacaip/GES_CoRoT_IACAIP_v0.fits", 1), None, "UVES"),
-    "PHOTOMETRY-U": (load_node_data("data/photometry/GES_CoRoT_PhotTeff.fits", 1), None, "UVES"),
-    "PHOTOMETRY-G": (load_node_data("data/photometry/GES_CoRoT_PhotTeff.fits", 1), None, "GIRAFFE"),
-    "LUMBA-EXCITATION": (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_ExcTeff.fits", 1), None, "GIRAFFE"),
-    "LUMBA-H-ALPHA": (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_HalphaTeff.fits", 1), None, "UVES"),
-    "LUMBA-NLTE": (load_node_data("data/lumba-nlte/GES_iDR3_WG11_LUMBA-CoRoT.fits", 1), None, "UVES"),
-    "SOUSA-GIRAFFE": (load_node_data("data/sousa/Results_line_ratio_sousa-cleaned.fits", 1), None, "GIRAFFE"),
-    "SOUSA-UVES": (load_node_data("data/sousa/Results_line_ratio_sousa-cleaned.fits", 1), None, "UVES"),
-    "BOLOGNA": (load_node_data("data/bologna/Bologna_GES_CoRoTv2/GES_CoRoT_25May2014_Bologna_NEW-cleaned.fits", 1), None, "UVES"),
-    "VILNIUS": (load_node_data("data/vilnius/GES_Corot_iteration1_Vilnius.v11-corrected.fits", 1), None, "UVES"),
-    "EPINARBO-UVES": (load_node_data("data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014-v2.fits", 1), None, "UVES"),
-    "EPINARBO-GIRAFFE": (load_node_data("data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014-v2.fits", 1), None, "GIRAFFE"),
-}
-
 uves_results = OrderedDict([
-    ("ARCETRI", (load_node_data("data/arcetri/GES_CoRoT_Arcetri_iter1.fits", 1), None, "UVES")),
-    ("IACAIP", (load_node_data("data/iacaip/GES_CoRoT_IACAIP_v0.fits", 1), None, "UVES")),
-    ("PHOTOMETRY", (load_node_data("data/photometry/GES_CoRoT_PhotTeff.fits", 1), None, "UVES")),
-    ("LUMBA (H-ALPHA)", (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_HalphaTeff.fits", 1), None, "UVES")),
-    ("LUMBA (NLTE)", (load_node_data("data/lumba-nlte/GES_iDR3_WG11_LUMBA-CoRoT.fits", 1), None, "UVES")),
-    ("SOUSA", (load_node_data("data/sousa/Results_line_ratio_sousa-cleaned.fits", 1), None, "UVES")),
-    ("BOLOGNA", (load_node_data("data/bologna/Bologna_GES_CoRoTv2/GES_CoRoT_25May2014_Bologna_NEW-cleaned.fits", 1), None, "UVES")),
-    ("VILNIUS", (load_node_data("data/vilnius/GES_Corot_iteration1_Vilnius.v11-corrected.fits", 1), None, "UVES")),
-    ("EPINARBO", (load_node_data("data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014-v2.fits", 1), None, "UVES")),
+    ("BOLOGNA", (load_node_data("data/bologna/Bologna_GES_CoRoTv2/GES_CoRoT_25May2014_Bologna_NEW-cleaned.fits", 1), "INVALID", "UVES")), # Those with TECH flags 10001A, 10002A, 10003A
+    ("LUMBA (H-$\\alpha$)", (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_HalphaTeff-cleaned.fits", 1), "INVALID", "UVES")), # 11110A, 10023A|11110A, 'double line or emission', 'HR15 not RV-corrected correctly'
+    ("VILNIUS", (load_node_data("data/vilnius/GES_Corot_iteration1_Vilnius.v11-corrected.fits", 1), "NONE_MARKED_AS_INVALID", "UVES")), # None INVALID
+    ("EPINARBO", (load_node_data("data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014-v2.fits", 1), "NONE_MARKED_AS_INVALID", "UVES")), # None INVALID
+    ("LUMBA (NLTE)", (load_node_data("data/lumba-nlte/GES_iDR3_WG11_LUMBA-CoRoT.fits", 1), "NONE_MARKED_AS_INVALID", "UVES")), # None INVALID
+    ("SOUSA", (load_node_data("data/sousa/Results_line_ratio_sousa-cleaned.fits", 1), "INVALID", "UVES")), # Those outside of their regime (as determined by them, not other nodes)
+    ("PHOTOMETRY", (load_node_data("data/photometry/GES_CoRoT_PhotTeff.fits", 1), "NONE_MARKED_AS_INVALID", "UVES")), # None INVALID
+    ("IACAIP", (load_node_data("data/iacaip/GES_CoRoT_IACAIP_v0.fits", 1), "NONE_MARKED_AS_INVALID", "UVES")),  # None INVALID
+    ("ARCETRI", (load_node_data("data/arcetri/GES_CoRoT_Arcetri_iter1.fits", 1), "INVALID", "UVES")), # TECH flag of 1 or 2
 ])
 
 giraffe_results = OrderedDict([
-    ("ARCETRI", (load_node_data("data/arcetri/GES_CoRoT_Arcetri_iter1.fits", 1), None, "GIRAFFE")),
-    ("PHOTOMETRY", (load_node_data("data/photometry/GES_CoRoT_PhotTeff.fits", 1), None, "GIRAFFE")),
-    ("LUMBA (H-ALPHA)", (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_HalphaTeff.fits", 1), None, "GIRAFFE")),
-    ("LUMBA (EXCITATION)", (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_ExcTeff.fits", 1), None, "GIRAFFE")),
-    ("SOUSA", (load_node_data("data/sousa/Results_line_ratio_sousa-cleaned.fits", 1), None, "GIRAFFE")),
-    ("EPINARBO", (load_node_data("data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014-v2.fits", 1), None, "GIRAFFE")),
+    ("LUMBA (H-$\\alpha$)", (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_HalphaTeff-cleaned.fits", 1), "INVALID", "GIRAFFE")), # 11110A, 10023A|11110A, 'double line or emission', 'HR15 not RV-corrected correctly'
+    ("LUMBA (EXCITATION)", (load_node_data("data/lumba/25june/GES_CoRoT_25May2014_LUMBA_ExcTeff-cleaned.fits", 1), "INVALID", "GIRAFFE")), # 10023A, 'Poor fit - binary?', 'HR15N not RV correctly'
+    ("EPINARBO", (load_node_data("data/epinarbo/Results_CoRoT_Epinarbo_18June2014/GES_CoRoT_Epinarbo_18June2014-v2.fits", 1), "NONE_MARKED_AS_INVALID", "GIRAFFE")), # None INVALID
+    ("SOUSA", (load_node_data("data/sousa/Results_line_ratio_sousa-cleaned.fits", 1), "INVALID", "GIRAFFE")), # Those outside of their regime (as determined by them, not other nodes)
+    ("PHOTOMETRY", (load_node_data("data/photometry/GES_CoRoT_PhotTeff.fits", 1), "NONE_MARKED_AS_INVALID", "GIRAFFE")), # None INVALID
+    ("ARCETRI", (load_node_data("data/arcetri/GES_CoRoT_Arcetri_iter1.fits", 1), "INVALID", "GIRAFFE")), # TECH flag of 1 or 2
 ])
 
 assert len(set([each[2] for each in uves_results.values()])) == 1
@@ -137,4 +115,5 @@ benchmark_truths = np.core.records.fromarrays([bm_object, bm_teff, bm_u_teff, bm
 
 initial_parameters = pyfits.open("data/initial_parameters.fits")[1].data
 
-validate_all()
+[validate(node, uves_results) for node in uves_results.keys()]
+[validate(node, giraffe_results) for node in giraffe_results.keys()]
